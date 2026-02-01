03b7b825c0d258b61ff0f6b06dd8517b4d08b99b
Gnalen Mara <gnalenmara80@gmail.com>
2026-01-31 22:27:35 -0500

    Replace list item with binoculars

---
+++ Lab3_username_replace.py
@@ -0,0 +1,7 @@
+#program name: Lab3_username_replace.py
+#author: Gnelen Mara
+#purpose: a program that will replace an item in the camping listwith new item
+#starter code: import the list from Lab3_gnalen_list.py
+#date: 01/31/2026
+from Lab3_gnalen_list import camping_items
+#this code will replace items in the camping list
