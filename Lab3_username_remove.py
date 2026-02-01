f369289035abe056a4fb4a2dfaf12a95541f474e
Gnalen Mara <gnalenmara80@gmail.com>
2026-01-31 23:44:04 -0500

    I cleaned the list by removing binoculars and printed final results

--- Lab3_username_remove.py
+++ Lab3_username_remove.py
@@ -4,4 +4,11 @@
 #starter code: import the list from Lab3_gnalen_add.py
 #date: 01/31/2026
 from Lab3_gnalen_add import camping_items
+
 #this code will remove the last item from the camping list
+if "binoculars" in camping_items: 
+    camping_items.remove("binoculars")
+
+    #print out the final list of items to take on the camping trip
+    print("final list", camping_items)
+    print(f"Total items for the camping trip: {len(camping_items)}")
