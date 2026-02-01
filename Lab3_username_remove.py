c620c72a7c9d3f6fbd47ab357742b716bcc749e3
Gnalen Mara <gnalenmara80@gmail.com>
2026-01-31 23:33:03 -0500

    imported list from Part 3

---
+++ Lab3_username_remove.py
@@ -0,0 +1,7 @@
+#program name: Lab3_username_remove.py
+#author: Gnalen Mara
+#purpose: a program that will remove the last item from the camping list
+#starter code: import the list from Lab3_gnalen_add.py
+#date: 01/31/2026
+from Lab3_gnalen_add import camping_items
+#this code will remove the last item from the camping list
