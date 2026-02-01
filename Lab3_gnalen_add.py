3397fd4f80e1aca981b6d0401a6f957e1e11bbde
Gnalen Mara <gnalenmara80@gmail.com>
2026-01-31 22:10:30 -0500

    part2 list updated

--- Lab3_gnalen_add.py
+++ Lab3_gnalen_add.py
@@ -3,11 +3,16 @@
 #purpose: a program that will add 5 items to an existing list of camping items
 #starter code: import the list from Lab3_gnalen_list.py
 #Date: 01/31/2026
+
 from Lab3_gnalen_list import camping_items
 
 #added 5 more items to the camping list
 camping_items.append("Stove")
-camping_items.append("rope")
-camping_items.append("gloves")
-camping_items.append("sunscreen")
-camping_items.append("knife")
+camping_items.append("Rope")
+camping_items.append("Gloves")
+camping_items.append("Sunscreen")
+camping_items.append("Knife")
+
+#print the updated list
+print(sorted(camping_items, reverse=True))
+
