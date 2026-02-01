edab43a78251997e56824ba8d503445e56c37f48
Gnalen Mara <gnalenmara80@gmail.com>
2026-01-31 20:46:32 -0500

    added  list of camping items

---
+++ Lab3_gnalen_list.py
@@ -0,0 +1,17 @@
+#program name: Lab3_gnalen_list.py
+#Author: Gnalen Mara
+#purpose: create a program with a list of strings containing exactly 10 camping items, that will output the list in alphabetical order, and output the number of items in the list.
+#starter code: none
+#Date: 01/31/2026
+camping_items = [
+    "Tent",
+    "Sleeping Bag",
+    "First Aid Kit",
+    "Falshlight"
+    "blanket"
+    "Water"
+    "Food"
+    "Map"
+    "Compass"
+    "Insect Repellent"
+]
